769deb676bcf9fd970aa52cc08fb0308828bc7be
frexxx-7 <borzenkokiril9@gmail.com>
2023-11-30 12:25:00 +0300

    admin panel

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -8,12 +8,17 @@ class AdminController extends Controller
 {
     public function adminInfo()
     {
+      try {
         $data = file_get_contents('storage/adminInfo.txt');
         $admin_data = explode("\n", $data);
         $username = trim($admin_data[0]);
         $email = trim($admin_data[1]);
         $password = trim($admin_data[2]);
         return response(compact('username','email', 'password'));
+      } catch (\Throwable $th) {
+        return response($th->getMessage());
+      }
+       
     }
 
 }
--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,40 +12,45 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
 
-    public function signup(SignupRequest $request)
-    {
-        $data = $request->validated();
-        /** @var \App\Models\User $user */
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
+  public function signup(SignupRequest $request)
+  {
+    $data = $request->validated();
+    /** @var \App\Models\User $user */
+    $user = User::create([
+      'name' => $data['name'],
+      'email' => $data['email'],
+      'password' => bcrypt($data['password']),
+    ]);
 
-        $token = $user->createToken('main')->plainTextToken;
-        return response(compact('user', 'token'));
+    $token = $user->createToken('main')->plainTextToken;
+    return response(compact('user', 'token'));
+  }
+
+  public function signin(LoginRequest $request)
+  {
+    try {
+      $credentials = $request->validated();
+      if (!Auth::attempt($credentials)) {
+        return response([
+          'message' => 'Provided email or password is incorrect'
+        ], 422);
+      }
+
+      /** @var \App\Models\User $user */
+      $user = Auth::user();
+      $token = $user->createToken('main')->plainTextToken;
+      return response(compact('user', 'token'));
+    } catch (\Throwable $th) {
+      return response($th->getMessage());
     }
 
-    public function signin(LoginRequest $request)
-    {
-        $credentials = $request->validated();
-        if (!Auth::attempt($credentials)) {
-            return response([
-                'message' => 'Provided email or password is incorrect'
-            ], 422);
-        }
+  }
 
-        /** @var \App\Models\User $user */
-        $user = Auth::user();
-        $token = $user->createToken('main')->plainTextToken;
-        return response(compact('user', 'token'));
-    }
-
-    public function logout(Request $request)
-    {
-        /** @var \App\Models\User $user */
-        $user = $request->user();
-        $user->currentAccessToken()->delete();
-        return response('', 204);
-    }
+  public function logout(Request $request)
+  {
+    /** @var \App\Models\User $user */
+    $user = $request->user();
+    $user->currentAccessToken()->delete();
+    return response('', 204);
+  }
 }
